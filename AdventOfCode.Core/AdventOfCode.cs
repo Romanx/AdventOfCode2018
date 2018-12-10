334d7704393545423e5172600d5297db0fbdde36
Alex McAuliffe <mcauliffe.alex@gmail.com>
2018-12-10 08:26:01 +0000

    Add core library for reading file etc

---
+++ AdventOfCode.Core/AdventOfCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode.Core
+{
+    public abstract class DayBase
+    {
+        private readonly string _inputFile;
+        protected string[] Lines => File.ReadAllLines(_inputFile, Encoding.Default);
+        protected string AllText => File.ReadAllText(_inputFile, Encoding.Default);
+
+        protected DayBase(string inputFile)
+        {
+            _inputFile = inputFile;
+        }
+
+        public static void PrintHeader(int number)
+        {
+            var part = $"[Part {number}]";
+            var sides = (Console.WindowWidth / 2) - part.Length;
+            var side = new string('-', sides);
+
+            Console.WriteLine($"{side}{part}{side}");
+        }
+
+        public abstract void Run();
+    }
+}
